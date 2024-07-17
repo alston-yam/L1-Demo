8e699a620b3b6d7be4ee0030dba14dc828f54008
ayrwl <alston.yam@gmail.com>
2024-07-17 23:36:22 +1200

    alsdf

--- L1 Demo/ContentView.swift
+++ L1 Demo/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("street")
+                    .resizable()
+                    .cornerRadius(12)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                Text("Street in Japan").font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).fontWeight(.semibold).foregroundColor(Color.white)
+            }
         }
-        .padding()
     }
 }
 
